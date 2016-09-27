ddaf1f4bec8e7d61dfdf258a9c22a950473e6dd9
Shieun Song <tldms0940@naver.com>
2016-09-27 20:28:58 +0900

    #4 make button

--- app/src/main/java/kr/hs/emirim/tldms0940/imagetest/MyImage.java
+++ app/src/main/java/kr/hs/emirim/tldms0940/imagetest/MyImage.java
@@ -10,10 +10,16 @@ import android.view.View;
  * Created by appcreator32 on 2016. 9. 26..
  */
 public class MyImage extends View{
+    int choose=MainActivity.ORIGINAL;
+    Bitmap picture;
         MyImage(Context context)
         {
             super(context);
+            picture = BitmapFactory.decodeResource(getResources(),R.drawable.image);
         }
+    public void setChoose(int choose){
+        this.choose=choose;
+    }
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
             Bitmap picture = BitmapFactory.decodeResource(getResources(), R.drawable.image);
@@ -24,8 +30,16 @@ public class MyImage extends View{
             //canvas.rotate(45,cx,cy);회전
             //canvas.translate(-150,200);이동
             //canvas.scale(0.5f,0.5f,cx,cy);크기
-            canvas.skew(0.4f, 0.4f);
+            if(choose==MainActivity.ROTATE)
+                canvas.rotate(90,cx,cy);
+            else if(choose==MainActivity.TRANSLATE)
+                canvas.translate(-150,200);
+            else if(choose==MainActivity.SCALE)
+                canvas.scale(0.5f,0.5f,cx,cy);
+            else if(choose==MainActivity.SKEW)
+                canvas.skew(0.4f, 0.4f);
 
             canvas.drawBitmap(picture, x, y, null);
+            //picture.recycle();
         }
 }
